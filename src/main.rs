ca744f562a6e53cbc47984ea41fa725db47e5ade
f3kilo <f3kilo@yandex.ru>
2023-02-19 08:15:44 +0300

    loop warning

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    loop {
+    while true {
         println!("Please input your guess.");
 
         let mut guess = String::new();
